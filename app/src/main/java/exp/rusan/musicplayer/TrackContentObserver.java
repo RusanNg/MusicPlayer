54483f1bacc4ef3c0beadf3b675f0bd123b28e03
Rusan <rusan.ng@qq.com>
2017-01-12 19:06:02 +0800

    加入TabLayout和ViewPager，改变主题，去除ToolBar阴影效果 TODO:加入MVP框架，权限管理，使用greenDAO管理播放列表等

--- app/src/main/java/exp/rusan/musicplayer/TrackContentObserver.java
+++ app/src/main/java/exp/rusan/musicplayer/TrackContentObserver.java
@@ -22,11 +22,11 @@ import java.util.List;
  * -->
  */
 
-public class MusicContentObserver extends ContentObserver{
+public class TrackContentObserver extends ContentObserver{
 
     private final String TAG = this.getClass().getSimpleName();
 
-    private List<SongBean> songList = new ArrayList<>();
+    private List<TrackBean> trackList = new ArrayList<>();
 
     private Context context;
     private Handler handler;
@@ -37,7 +37,7 @@ public class MusicContentObserver extends ContentObserver{
      *
      * @param pHandler The handler to run {@link #onChange} on, or null if none.
      */
-    public MusicContentObserver(Context pContext, Handler pHandler) {
+    public TrackContentObserver(Context pContext, Handler pHandler) {
         super(pHandler);
         context = pContext;
         handler = pHandler;
@@ -51,10 +51,11 @@ public class MusicContentObserver extends ContentObserver{
 
         Uri musicContentUri = Media.EXTERNAL_CONTENT_URI;
 
-        songList = MusicLoader.getInstance(context.getContentResolver()).getReloadSongList();
+        trackList = TrackLoader.getInstance(context.getContentResolver()).getReloadTrackList();
 
-//        Log.i(TAG, "song list num :　" + songList.size());
-        handler.obtainMessage(MusicListActivity.whatMusicContentObserver, songList).sendToTarget();
+//        Log.i(TAG, "song list num :　" + trackList.size());
+        handler.obtainMessage(LibraryPageFragment.WHAT_TRACK_CONTENT_OBSERVER, trackList)
+                .sendToTarget();
 
     }
 }
