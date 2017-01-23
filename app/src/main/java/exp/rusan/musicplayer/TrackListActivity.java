744e1a8b942d2ed8da66aa193cc6991bcb74680e
Rusan <rusan.ng@qq.com>
2017-01-23 17:58:13 +0800

    整合TrackContentResolver和TrackContentObserver到TrackLoad TODO:使用greenDAO管理播放列表等

--- app/src/main/java/exp/rusan/musicplayer/TrackListActivity.java
+++ app/src/main/java/exp/rusan/musicplayer/TrackListActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -26,27 +28,13 @@ public class TrackListActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_list);
 
-        int[] requestPermissions = new int[] {
-            PermissionUtils.CODE_CALL_PHONE,
-            PermissionUtils.CODE_CAMERA,
-            PermissionUtils.CODE_GET_ACCOUNTS
-
-        };
-
-
         // TODO: 2017/1/6 权限管理
         PermissionUtils.requestPermission(this, PermissionUtils.CODE_READ_EXTERNAL_STORAGE,
                 permissionGrant);
 
-//        PermissionUtils.requestMultiPermissions(this, requestPermissions, permissionGrant);
+    }
 
-//        ActivityCompat.requestPermissions(this, new String[]{
-//                Manifest.permission.CALL_PHONE,
-//                Manifest.permission.READ_EXTERNAL_STORAGE,
-//                Manifest.permission.GET_ACCOUNTS
-//        }, 1);
-
-
+    void setUpView() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setElevation(0);
@@ -61,20 +49,16 @@ public class TrackListActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-//        // TabLayout and ViewPager
-//        ViewPager vpLibrary = (ViewPager) findViewById(R.id.vp_library);
-//
-//        LibraryFragmentPagerAdapter vpLibraryAdapter = new LibraryFragmentPagerAdapter
-//                (getSupportFragmentManager(), this);
-//        vpLibrary.setAdapter(vpLibraryAdapter);
-//
-//        TabLayout tlLibrary = (TabLayout) findViewById(R.id.tl_library);
-//        tlLibrary.setupWithViewPager(vpLibrary);
+        // TabLayout and ViewPager
+        ViewPager vpLibrary = (ViewPager) findViewById(R.id.vp_library);
 
+        LibraryFragmentPagerAdapter vpLibraryAdapter = new LibraryFragmentPagerAdapter
+                (getSupportFragmentManager(), this);
+        vpLibrary.setAdapter(vpLibraryAdapter);
 
-
+        TabLayout tlLibrary = (TabLayout) findViewById(R.id.tl_library);
+        tlLibrary.setupWithViewPager(vpLibrary);
     }
-
 
     private PermissionUtils.PermissionGrant permissionGrant = new PermissionUtils.PermissionGrant() {
         @Override
@@ -84,6 +68,7 @@ public class TrackListActivity extends AppCompatActivity
                 case PermissionUtils.CODE_READ_EXTERNAL_STORAGE:
                     Snackbar.make(findViewById(R.id.toolbar), "Permission requested is " +
                             "granted!!!", Snackbar.LENGTH_SHORT).show();
+                    setUpView();
                     break;
 
                 default:
@@ -91,6 +76,8 @@ public class TrackListActivity extends AppCompatActivity
             }
         }
     };
+
+
 
 
     @Override
